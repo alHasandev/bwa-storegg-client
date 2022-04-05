a547f3c242dbe35218acf9874b7e53446d404717
alHasandev <alhasan99dev@gmail.com>
2022-04-05 10:41:40 +0800

    Hooks: Add localStorage use hook

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,39 @@
+/* eslint-disable comma-dangle */
+import { useEffect, useState } from 'react';
+
+type InitialValue = () => string;
+
+type GetJSON = (
+  // eslint-disable-next-line no-unused-vars
+  key: string,
+  // eslint-disable-next-line no-unused-vars
+  initialValue: InitialValue | any
+) => any;
+
+const getSavedValue: GetJSON = (
+  key: string,
+  initialValue: InitialValue | any
+) => {
+  const savedValue = JSON.parse(localStorage.getItem(key) || '');
+  if (savedValue) return savedValue;
+  if (initialValue instanceof Function) return initialValue();
+
+  return initialValue;
+};
+
+export default function useLocalStorage(
+  key: string,
+  initialValue: InitialValue | any
+) {
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    setValue(() => getSavedValue(key, initialValue ?? ''));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
+
+  return { localValue: value, saveLocalValue: setValue };
+}
