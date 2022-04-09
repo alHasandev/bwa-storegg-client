f693d59838f3e165735c5bd62de39a7d08f52ed8
alHasandev <alhasan99dev@gmail.com>
2022-04-09 18:19:22 +0800

    Fix: Integrating transactions page

--- components/organisms/TransactionsContent/index.tsx
+++ components/organisms/TransactionsContent/index.tsx
@@ -1,15 +1,40 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable object-curly-newline */
+import { IMAGE_URL } from '../../../services';
+import { TDashboard, useDashboard } from '../../../services/players';
 import rupiah from '../../../utilities/Intl/rupiah';
 import TagButton from '../../atoms/TagButton';
+import { TransactionRecordProps } from '../OverviewContent/types';
 import TableRow from './TableRow';
 
-function TransactionsContent() {
+type TransactionsContentProps = {
+  jwtToken: string;
+};
+
+function TransactionsContent({ jwtToken }: TransactionsContentProps) {
+  const { data, error } = useDashboard(jwtToken);
+
+  if (!data && !error) return <div>Loading...</div>;
+  if (error) return <div>Error!, hubungi admin</div>;
+
+  const { totalSpent, transactions }: TDashboard = data;
+
+  const transactionsTotal = Object.values(totalSpent).reduce(
+    (prev, current) => prev + current,
+    0
+  );
+
   return (
     <main className="main-wrapper">
       <div className="ps-lg-0">
-        <h2 className="text-4xl fw-bold color-palette-1 mb-30">My Transactions</h2>
+        <h2 className="text-4xl fw-bold color-palette-1 mb-30">
+          My Transactions
+        </h2>
         <div className="mb-30">
           <p className="text-lg color-palette-2 mb-12">You’ve spent</p>
-          <h3 className="text-5xl fw-medium color-palette-1">{rupiah(4518000500)}</h3>
+          <h3 className="text-5xl fw-medium color-palette-1">
+            {rupiah(transactionsTotal)}
+          </h3>
         </div>
         <div className="row mt-30 mb-20">
           <div className="col-lg-12 col-12 main-content">
@@ -22,7 +47,9 @@ function TransactionsContent() {
           </div>
         </div>
         <div className="latest-transaction">
-          <p className="text-lg fw-medium color-palette-1 mb-14">Latest Transactions</p>
+          <p className="text-lg fw-medium color-palette-1 mb-14">
+            Latest Transactions
+          </p>
           <div className="main-content main-content-table overflow-auto">
             <table className="table table-borderless">
               <thead>
@@ -37,46 +64,29 @@ function TransactionsContent() {
                 </tr>
               </thead>
               <tbody id="list_status_item">
-                <TableRow
-                  game={{
-                    img: '/img/overview-1.png',
-                    title: 'Mobile Legends: The New Battle 2021',
-                    category: 'Desktop',
-                  }}
-                  item="200 Gold"
-                  price={290000}
-                  status="pending"
-                />
-                <TableRow
-                  game={{
-                    img: '/img/overview-2.png',
-                    title: 'Call of Duty:Modern',
-                    category: 'Desktop',
-                  }}
-                  item="550 Gold"
-                  price={740000}
-                  status="success"
-                />
-                <TableRow
-                  game={{
-                    img: '/img/overview-3.png',
-                    title: 'Clash of Clans',
-                    category: 'Mobile',
-                  }}
-                  item="100 Gold"
-                  price={120000}
-                  status="failed"
-                />
-                <TableRow
-                  game={{
-                    img: '/img/overview-4.png',
-                    title: 'The Royal Game',
-                    category: 'Mobile',
-                  }}
-                  item="225 Gold"
-                  price={200000}
-                  status="pending"
-                />
+                {transactions.map((transaction) => {
+                  const { historyVoucherTopup: gameData } = transaction;
+                  const { game, item, price, status }: TransactionRecordProps =
+                    {
+                      game: {
+                        img: `${IMAGE_URL}/${gameData.thumbnail}`,
+                        title: gameData.gameName,
+                        category: gameData.category,
+                      },
+                      item: `${gameData.coinQuantity} ${gameData.coinName}`,
+                      price: transaction.value,
+                      status: transaction.status,
+                    };
+                  return (
+                    <TableRow
+                      key={transaction._id}
+                      game={game}
+                      item={item}
+                      price={price}
+                      status={status}
+                    />
+                  );
+                })}
               </tbody>
             </table>
           </div>
--- pages/member/transactions/index.tsx
+++ pages/member/transactions/index.tsx
@@ -1,12 +1,47 @@
-import type { NextPage } from 'next';
+import jwtDecode from 'jwt-decode';
+import type { GetServerSideProps, NextPage } from 'next';
 import Sidebar from '../../../components/organisms/Sidebar';
 import TransactionsContent from '../../../components/organisms/TransactionsContent';
+import { JwtData } from '../../../services/auth';
 
-const Transactions: NextPage = () => (
+type TransactionsProps = {
+  jwtToken: string;
+};
+
+const Transactions: NextPage<TransactionsProps> = ({
+  jwtToken,
+}: TransactionsProps) => (
   <section className="transactions overflow-auto">
     <Sidebar activeMenu="transactions" />
-    <TransactionsContent />
+    <TransactionsContent jwtToken={jwtToken} />
   </section>
 );
 
 export default Transactions;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  try {
+    const { token } = req.cookies;
+
+    if (!token) throw new Error('Silahkan login terlebih dahulu!');
+
+    const jwtToken = Buffer.from(token, 'base64').toString('ascii');
+    const { player: user }: JwtData = jwtDecode(jwtToken);
+
+    if (!user) throw new Error('Silahkan login terlebih dahulu!');
+
+    return {
+      props: {
+        user,
+        jwtToken,
+      },
+    };
+  } catch (error) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false,
+      },
+    };
+  }
+};
