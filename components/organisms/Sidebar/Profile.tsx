c96da9834cc93bd96b7060aa68156948d33ad831
alHasandev <alhasan99dev@gmail.com>
2022-04-10 14:32:21 +0800

    Fix: Sidebar profile image

--- components/organisms/Sidebar/Profile.tsx
+++ components/organisms/Sidebar/Profile.tsx
@@ -1,5 +1,6 @@
 import Cookies from 'js-cookie';
 import jwtDecode from 'jwt-decode';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { IMAGE_URL } from '../../../services';
 import { JwtData } from '../../../services/auth';
@@ -33,13 +34,14 @@ function Profile() {
 
   return (
     <div className="user text-center pb-50 pe-30">
-      <img
-        src={`${IMAGE_URL}/${user.avatar}`}
-        width="90"
-        height="90"
-        className="img-fluid mb-20"
-        alt="avatar"
-      />
+      <div className="preview-image sidebar-image">
+        <Image
+          src={`${IMAGE_URL}/${user.avatar}`}
+          objectFit="cover"
+          alt="upload icon"
+          layout="fill"
+        />
+      </div>
       <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
       <p className="color-palette-2 m-0">{user.email}</p>
     </div>
